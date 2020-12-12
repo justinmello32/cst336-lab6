77689142a3c11cda7584ce9bda922ed49c6d8126
Justin Mello <justinmello32@hotmail.com>
2020-12-12 05:07:09 +0000

    Finished part 5.

--- app.js
+++ app.js
@@ -19,17 +19,28 @@ app.get("/", function(req, res){
     res.render("index");
 });
 
-app.post("/", function(req,res){
+app.post("/", async function(req,res){
     let username = req.body.username;
     let password = req.body.password;
-    console.log("username:" + username);
-    console.log("password:" + password);
+    
+    let hashedPwd = "$2a$10$06ofFgXJ9wysAOzQh0D0..RcDp1w/urY3qhO6VuUJL2c6tzAJPfj6";
+    let passwordMatch = await checkPassword(password, hashedPwd);
+    
     if(username =='admin' && password=='secret') {
         res.render("welcome");
     } else {
         res.render("index", {"loginError":true});
     }
 })
+
+function checkPassword(password, hashedValue) {
+    return new Promise( function(resolve, reject){
+        bcrypt.compare(password, hashedValue, function(err, result){
+            console.log("Result: " + result);
+            resolve(result);
+        })
+    })
+}
 
 //Listener
 app.listen(8080, "0.0.0.0", function(){
